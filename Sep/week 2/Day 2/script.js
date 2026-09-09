612807f71c3970507f71a1f3c8b8c626039b994d
pratikshyasahoo-creator <pratikshyasahooguddy@gmail.com>
2026-09-09 19:44:15 +0530

    Variables,Datatypes

--- Sep/week 2/Day 2/script.js
+++ Sep/week 2/Day 2/script.js
@@ -1,4 +1,58 @@
-console.log("Hello, World!");
+console.log("Welcome");
 
+a=20;
+console.log(a);
+
+A=30;
+console.log(A);
+
+n1ame="hii";
+console.log(n1ame);
+
+full_name="kjshdkjash";
+console.log(full_name);
+
+std$email="std@gmail.com"
+console.log(std$email)
+
+// console.log(b);
+var b=40;
 console.log(b);
-b=5;
+
+// way of declaration
+// where you declare and where you can access (scope of variable)
+
+// 3 scope
+// global scope
+// block scope
+// functional scope
+
+//  declare the variable using var keyword in block
+{
+    var name="xyz";
+}
+console.log(name)
+
+//  declare the variable using var keyword in func
+function fun(){
+ var age=50;
+}
+console.log(age)
+fun()
+
+
+
+
+{
+    let name="xyz";
+    console.log(name)
+}
+
+
+//  declare the variable using var keyword in func
+function fun(){
+ let age=50;
+ console.log(age)
+}
+
+fun()
